739f6454dc6bb25c0e3d1b09c3ee6e1751a2efc1
admin <admin@DESKTOP-M1PMCQ8>
2022-10-01 10:16:33 +0700

    update

--- web65/Controllers/PersonController.cs
+++ web65/Controllers/PersonController.cs
@@ -24,9 +24,23 @@ namespace web65.Controllers
             //return View("Index", persons); // giu nguyen url, nhung gui lai request (add them) khi f5 (bi duplicate.)
             return RedirectToAction("Index", persons);//thay doi url va k bi duplicate.
         }
+        [HttpPost]
         public IActionResult Delete(int id)
         {
             persons.RemoveAt(id);
+            return RedirectToAction("Index", persons);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var person = persons[id];
+            ViewBag.Id = id;
+            return View(person);
+        }
+        [HttpPost]
+        public IActionResult Edit(Person person, int id)
+        {
+            persons[id] = person;
             return RedirectToAction("Index", persons);
         }
     }
