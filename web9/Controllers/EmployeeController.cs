31625ed861b5b7a75e979c56b4150b16e2cbb005
admin <admin@DESKTOP-M1PMCQ8>
2022-10-06 09:56:49 +0700

    update

---
+++ web9/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web9.Models;
+
+namespace web9.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", employee);
+            }
+            return View(employee);
+        }
+        public IActionResult Result(Employee employee)
+        {
+            return View(employee);
+        }
+    }
+}
---
+++ web9/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace web9.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "required r")]
+        [StringLength(100, ErrorMessage = "Ten ngan qua", MinimumLength = 5)]
+        [Display(Name = "Nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Position { get; set; }
+        [Required(ErrorMessage = "required r")]
+        [DataType(DataType.Date)]
+        public DateTime DoB { get; set; }
+        [Range(100, 3000, ErrorMessage = "Luong tu 100-3000")]
+        public double Salary { get; set; }
+
+    }
+}
