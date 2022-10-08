e7f8e112dbedb600fdbebe7fc6f3fdae9096c889
admin <admin@DESKTOP-M1PMCQ8>
2022-10-08 08:47:28 +0700

    update

---
+++ web11/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace web11.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "9 char")]
+        [Required]
+        public string StudentId { get; set; }
+        [MinLength(2)]
+        [Required]
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime DoB { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Format invalid")]
+        public string Email { get; set; }
+        [Required]
+        [Range(0,10)]
+        public double Grade { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ web11/Validation/DateValidate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace web11.Validation
+{
+    public class DateValidate : ValidationAttribute
+    {
+        private const string DateTimeFormat = "dd/MM/yy";
+        private readonly DateTime date;
+        private string today = DateTime.Now.Date.ToShortDateString();
+        public DateValidate()
+        {
+            this.date = DateTime.ParseExact(today, DateTimeFormat, CultureInfo.InvariantCulture);
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if ((DateTime)value >= date) return new ValidationResult(this.ErrorMessage);
+            return ValidationResult.Success;
+        }
+    }
+}
