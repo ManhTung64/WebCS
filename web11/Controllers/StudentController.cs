4cf06a3092d6fc68395f5703ce5cb41d0a3a8a50
admin <admin@DESKTOP-M1PMCQ8>
2022-10-08 10:10:51 +0700

    update

--- web11/Controllers/StudentController.cs
+++ web11/Controllers/StudentController.cs
@@ -16,5 +16,27 @@ namespace web11.Controllers
             var data = context.students.ToList();
             return View(data);
         }
+        public IActionResult Detail(int? id)
+        {
+            if (id == null) return NotFound();
+            var product = context.students.FirstOrDefault(student => student.Id == id);
+            return View(product);
+        }
+        public IActionResult Edit(int? id)
+        {
+            return View("Index");
+        }
+        public IActionResult Add(int? id)
+        {
+            return View("Index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)return NotFound();
+            var student = context.students.Find(id);
+            context.students.Remove(student);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
