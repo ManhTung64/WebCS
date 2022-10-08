6c97e19c768d9abf44abb3f4d34fb2a00acdc98d
admin <admin@DESKTOP-M1PMCQ8>
2022-10-08 09:32:37 +0700

    update

---
+++ web11/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using web11.Data;
+
+namespace web11.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public StudentController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = context.students.ToList();
+            return View(data);
+        }
+    }
+}
